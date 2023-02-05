ef7d354fe001ffd6601b8159dad489d257e300de
Sanjog Regmi <mailsanjog.regmi@gmail.com>
2023-02-05 08:25:20 +0545

    intial commit

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,7 +2,7 @@ import bcryptjs from 'bcryptjs';
 
 const users = [
     { name: 'Admin', email: 'admin@proshop.com', password: bcryptjs.hashSync('password'), isAdmin: true },
-    { name: 'Piyush Saha', email: 'piyush@example.com', password: bcryptjs.hashSync('password')},
+    { name: 'Sanjog', email: 'sanjog@example.com', password: bcryptjs.hashSync('password')},
     { name: 'John Doe', email: 'john@exmaple.com', password: bcryptjs.hashSync('password')}
 ];
 
